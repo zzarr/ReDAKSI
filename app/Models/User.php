76c227c6c3e405ca46bf8312817859fdfd3f891d
zal77 <rizalfauzi7704@gmail.com>
2023-11-16 11:15:36 +0700

    mencoba breeze auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
@@ -42,4 +41,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected $table = 'user';
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,17 +20,12 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/Login', function () {
-    return view(
-        'login',
-        [
-            'title' => "Login"
-        ]
-    );
-});
-Route::get('/add_account', function()
-{
-    return view('tambahAkun');
-});
+require __DIR__.'/auth.php';
