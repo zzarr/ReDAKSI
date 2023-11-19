3878606987e04210bfcad0e6b1633c0941f06ec9
zzarr <azharsyah.curug@gmail.com>
2023-11-19 11:38:46 +0700

    vite bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,12 @@
+import loadash from 'lodash'
+window._ = loadash
+
+
+import * as Popper from '@popperjs/core'
+window.Popper = Popper
+
+import 'bootstrap'
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -8,6 +17,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,9 +5,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }} ">
-  <link rel="stylesheet" href="{{ asset('css/all.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  @vite(['resources/js/app.js'])
   <title>Dashboard</title>
 </head>
 
@@ -101,8 +99,7 @@
 
 
 
-  <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
-  <script src="{{ asset('js/all.js') }}"></script>
+ 
 </body>
 
 </html>
