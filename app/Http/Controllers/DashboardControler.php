c1d98bcc1483d07fac0d075cb0465480f30fd5b7
zal77 <rizalfauzi7704@gmail.com>
2023-12-04 12:06:33 +0700

    terbaru

--- app/Http/Controllers/DashboardControler.php
+++ app/Http/Controllers/DashboardControler.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\DB;
 
 class DashboardControler extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $webtitle = 'Dashboard';
         $arsip = DB::table('folders')->get();
-        return view('admin.dashboard' ,compact('arsip','webtitle'));
+        return view('admin.dashboard', compact('arsip', 'webtitle'));
     }
 }
