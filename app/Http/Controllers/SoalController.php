f5715b759285027bde6509c590d4213bdeb5d7fd
zzarr <azharsyah.curug@gmail.com>
2023-12-15 20:31:18 +0700

    up

--- app/Http/Controllers/SoalController.php
+++ app/Http/Controllers/SoalController.php
@@ -10,7 +10,29 @@ class SoalController extends Controller
     public function create($id){
         $webtitle = "Standar Akreditasi";
         $standar = DB::table('StandarAkreditasi');
-        return view('admin.Soal.add_soal',compact('id','webtitle','satndar'));
-        
+        $standar = DB::table('StandarAkreditasi')->get();
+        return view('admin.Soal.add_soal',compact('id','webtitle','standar'));
+
     }
+
+    public function insert(Request $request){
+        $skor = DB::table('StandarAkreditasi')->where('id',$request->input('id'))->first(['jumlahBobotButir']);
+        $skorbtr = $skor->jumlahBobotButir;
+        DB::table('SoalAkreditasi')->insert([
+            'idp'           => intval($request->input('id')),
+            'id_standar'    => $request->input('standar'),
+            'pertanyaan'    => $request->input('pertanyaan'),
+            'A'             => $request->input('A'),
+            'B'             => $request->input('B'),
+            'C'             => $request->input('C'),
+            'D'             => $request->input('D'),
+            'E'             => $request->input('E'),
+            'skor_butir'    => $skorbtr
+        ]);
+
+        return redirect("/admin/show/{$request->input('standar')}");
+    }
+
+    
 }
+
